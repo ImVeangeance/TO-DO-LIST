319021a6bae37e7ec7912206de8840189c698b2a
PlsDeleteMyLife <cdranton@gmail.com>
2018-05-29 05:03:18 -0400

    Add smth for tests

--- main.c
+++ main.c
@@ -5,8 +5,13 @@
 #include <locale.h>
 
 
-int Greeting();
+void Greeting();
 void Menu();
+int Looking();
+int Editing();
+int Creating();
+int Deleting();
+//int Adding(); - ?? nado li
 
 
 int main()
@@ -15,22 +20,22 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	Greeting();
 	Menu();
+	Creating();
 	system("pause"); // Need to del in last commit
 	return 0;
 }
 
-int Greeting()
+void Greeting()
 {
 	char NameOfUser[20];
 	// 
 	printf("\nЭлектронный ежедневник\n");
  	printf("Как мне к вам обращаться?\n[Используйте не более 20 символов на имя]\n"); 
-	//
 	scanf("%s", NameOfUser);
+	//Create the namesaver;
 	printf("\n\nПриветствую тебя, ");
 	printf("%s", NameOfUser);
 	printf("!\n");
-	return 0;
 }
 
 void Menu()
@@ -42,7 +47,7 @@ void Menu()
 	printf("|C - создать ");
 	printf("E - редактировать |\n");
 	printf("|D - удалить ");
-	printf("A - добавить      |\n");
+	printf("A - добавить дело |\n");
 	printf("|ESC - выйти                   |\n");
 	printf(" _____________________________\n\n\n");
 	printf("'Help' - команда для вывода меню\n\n");
@@ -61,51 +66,81 @@ void Menu()
 		printf("Дата(при желании), Заметка/Действие  , Приоритет:\n");
 		printf("08.08.2018       , Встреча с друзьями, Высокий приоритет.\n");
 		printf("_________________________________________________________\n\n\n");
-/*		printf("Press any key to continue\n");
-		while(!kbhit (0))
+		//Describe about all(not ended)
+		printf("Напишите 'хорошо' для продолжения\n");
+		char Beginer[6];
+		do
 		{
-			system("clear");
+			scanf("%s", Beginer);
 			system("clear");
 			Menu();
-		}*/
+		} while(!strcmp("хорошо", Beginer));
 	}
-	else 
-    {
-    if(!strcmp("L", Helper))
+     else 
+     {
+        if(!strcmp("L", Helper))
 	{
-		//
+		//Looking();
 	}
 	if(!strcmp("C", Helper))
 	{
-			//Creating();
+		//Creating();
 	}
 	if(!strcmp("E", Helper))
 	{
-		//
+		//Editing();
 	}
 	if(!strcmp("D", Helper))
 	{
-		//
+		//Deleting():
 	}
 	if(!strcmp("A", Helper))
 	{
-		//
+		//Adding();
 	}
 	if((!strcmp("ESC", Helper)) || (!strcmp("Esc", Helper)) || (!strcmp("esc", Helper)))
 	{
 		system("clear");
-//		break;
+		system("break");
 	}		
 	else //условие ошибки 
 	{
 		system("clear");
 		Menu();
 	}
-    }
+     }
 //	system("clear");	
 }
 
+int Creating()
+{
+	int count=1;
+	int i=0;
+	struct spis
+	{ 
+		int number;
+		char data[25];
+		char memo[140];
+		int priority;
+	};
+	struct spis note[count];
 
+	for(i=0;i<count;i++)
+	{
+		note[i].number=i++;
+		printf("Номер дела - №%d\n", note[i].number);
+
+		printf("Введите дату в удобном для вас формате:\n");
+		scanf("%s\n", note[i].data);
+
+		printf("Заметка:\n");
+		scanf("%s\n", note[i].memo);
+
+		printf("Введите приоритет заметки - [0;2], от большего к меньшему:\n");
+		scanf("%d\n", note[i].priority);
+	}
+	//
+}
 
 
 
