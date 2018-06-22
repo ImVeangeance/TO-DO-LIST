9b08b9c94c1a1523809ee9435a77630cfb32c10a
Nikita-Melnikov <melnikov73nikita@gmail.com>
2018-06-22 17:33:10 +0700

    Add header file

---
+++ src/function.h
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#ifndef function_h_
+#define function_h_
+#include <stdio.h>
+
+typedef struct Note{
+	
+	char memo[144];
+    int priority;
+		
+}Note;
+
+FILE *buffile;
+FILE *database;
+
+Note* InitNote(void);
+int Print(FILE *database);
+void View(FILE * database);
+void CreateOrAdd(FILE *database, char state);
+void Copy(FILE *database, FILE *buffile);
+void Edit(FILE *database, FILE *buffile);
+void Delete(FILE *database, FILE *buffile);
+void Menu(void);
+
+#endif
