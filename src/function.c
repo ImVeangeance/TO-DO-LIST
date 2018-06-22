cc7146bd0f480f70e17cc75647f5b38b8a5e3283
Nikita-Melnikov <melnikov73nikita@gmail.com>
2018-06-22 17:23:49 +0700

    Add Delete function

--- src/function.c
+++ src/function.c
@@ -110,7 +110,48 @@ void Copy(FILE *database, FILE *buffile){
     fclose(buffile);
 }
 
-
+void Delete(FILE *database, FILE *buffile){
+	
+	database = fopen("database.txt","rb");
+	buffile = fopen("buffile.txt","wb");
+	system("CLS");
+	
+	int num_del;                   
+	int num = Print(database);     
+	
+	if(num == 0)
+		return;
+	
+	printf("Input the number of note for deleting\n");
+	scanf("%d", &num_del);
+	
+	while(1){
+	    
+		if(num_del > 0 && num_del <= num)
+			break;
+		printf("Wrong number, input again\n");
+		scanf("%d", &num_del);
+	}
+	
+    Note* buff = InitNote();
+	fread(buff, sizeof(Note), 1, database);
+	
+	int i = 1;
+	while(!feof(database)){
+		
+		if(i != num_del)
+		    fwrite(buff, sizeof(Note), 1, buffile);
+		    
+		fread(buff, sizeof(Note), 1, database);
+		i++;
+	}
+	
+	free(buff);
+	fclose(database);
+    fclose(buffile);
+	
+    Copy(database, buffile);   
+}
 
 
 
