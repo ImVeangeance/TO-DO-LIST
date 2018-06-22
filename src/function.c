6e8be1571451a71768bb809e15a95135c91714c5
TimaFromSibGUCCI <gazitdinovtimur@mail.ru>
2018-06-22 17:31:25 +0700

    Add a second half of Edit function

--- src/function.c
+++ src/function.c
@@ -131,8 +131,66 @@ void Edit(FILE *database, FILE *buffile){
 		printf("Wrong number, input again\n");
 		scanf("%d", &num_edit);
 	}
-	////
+
+	printf("Input the cell for editing\n");
+	printf(" ________________________\n");
+	printf("|      P - priority     |\n");
+	printf("|      N - note         |\n");
+	printf("|_______________________|\n\n");
+	char ch = getch();
+	while(1){
+		
+		if(ch == 'P' || ch == 'N')
+			break;
+		printf("Wrong cell-name, input again\n");
+		ch = getch();
+	}
+	
+	
+    Note* buff = InitNote();
+	fread(buff, sizeof(Note), 1, database);
+	
+	int i = 1;
+	while(!feof(database)){
+		
+		if(i == num_edit){
+			
+		    int j;
+			unsigned char pr;
+			printf("Input new info\n");
+			switch(ch){
+
+			    case 'N': for(j = 0; j < 144; j++){
+		                       buff->memo[j] = 0;
+	                      }
+						  scanf("%142s", buff->memo);
+						  break;
+						 
+				case 'P': pr = getch();
+		                  while(1){
+			                  if((pr - '0') >= 1 && (pr - '0') <= 3){
+				                  buff->priority = (pr - '0');
+			                      break;
+			                  }	
+			                  printf("Wrong priority, input again\n");
+			                  pr = getch();
+		                  }
+						  break;
+			}
+		}
+		
+	    fwrite(buff, sizeof(Note), 1, buffile);        		
+		fread(buff, sizeof(Note), 1, database);
+		i++;
+	}
+	
+	free(buff);
+	fclose(database);
+    fclose(buffile);
+	
+    Copy(database, buffile);   
 }
+
 
 void Delete(FILE *database, FILE *buffile){
 	
