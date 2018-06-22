5fb0a38199ccd65702c79565d9b655b1e970fd4b
TimaFromSibGUCCI <gazitdinovtimur@mail.ru>
2018-06-22 17:22:25 +0700

    Add Copy function

--- src/function.c
+++ src/function.c
@@ -92,6 +92,24 @@ void CreateOrAdd(FILE * database, char state){
 	fclose(database);	
 }
 
+void Copy(FILE *database, FILE *buffile){
+	
+    database = fopen("database.txt","wb");
+	buffile = fopen("buffile.txt","rb");
+	
+	Note* buff = InitNote();
+	fread(buff, sizeof(Note), 1, buffile);
+	while(!feof(buffile)){
+		
+		fwrite(buff, sizeof(Note), 1, database);
+		fread(buff, sizeof(Note), 1, buffile);  
+	}
+	
+	free(buff);
+	fclose(database);
+    fclose(buffile);
+}
+
 
 
 
